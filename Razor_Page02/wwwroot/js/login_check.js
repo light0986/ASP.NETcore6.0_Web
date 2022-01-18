7d279a1b04823f1aa250c5674c8422f01efdf7c5
light0986 <82749910+light0986@users.noreply.github.com>
2022-01-18 16:10:26 +0800

    Delete Razor_Page02 directory

--- Razor_Page02/wwwroot/js/login_check.js
+++
@@ -1,6 +0,0 @@
-﻿var Acc = localStorage.getItem("KeyCode");
-
-function load_storge() {
-    var ls01 = localStorage.getItem("KeyCode");
-    document.getElementById("container").innerHTML = ls01;
-}
